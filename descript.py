15e0ca4945545678f1c7c6cca530b482b6d268b8
notwn <nandiinewt@gmail.com>
2024-10-11 09:31:29 +0300

    plotting of the data

--- descript.py
+++ descript.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load the dataset
 auto_dataset = "auto_dataset.csv"
@@ -7,3 +10,8 @@ df = pd.read_csv(auto_dataset)
 #Counting the number of cars in each category
 drive_wheels_counts= df["drive-wheels"].value_counts()
 print(drive_wheels_counts)
+# Convert the 'price' column to numeric
+df['price'] = pd.to_numeric(df['price'], errors='coerce')
+
+sns.boxplot(x="drive-wheels", y="price", data=df ,hue_order=["fwd","rwd","4wd"], palette=["blue","orange","green"])
+plt.show()
