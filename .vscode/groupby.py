eded903ec434c64d400ba67f107149f8b797884b
notwn <nandiinewt@gmail.com>
2024-10-15 12:28:13 +0300

    add file

---
+++ .vscode/groupby.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load the dataset
+auto_dataset = "auto_dataset.csv"
+df = pd.read_csv(auto_dataset)
+
+#finding the average price of cars and how they differ between different types of body-styles and drive-wheels
+df_test = df[["price", "body-style","drive-wheels"]]
+
+df_grp = df_test.groupby(["body-style","drive-wheels","price"], as_index=False).mean()
+print(df_grp)
